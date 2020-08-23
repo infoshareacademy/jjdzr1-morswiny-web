4952c06d078194b5f2d1df7fd59aece6d18aa250
Tomek Miecielica <tomek.miecielica@gmail.com>
2020-08-23 19:40:47 +0200

    Added logic to display 3 events on the main page

--- src/main/java/com/isa/morswiny/servlets/AllEventsListServlet.java
+++ src/main/java/com/isa/morswiny/servlets/AllEventsListServlet.java
@@ -5,6 +5,11 @@ import com.isa.morswiny.eventsDao.EventCRUDRepository;
 import com.isa.morswiny.eventsDao.EventCRUDRepositoryInterface;
 import com.isa.morswiny.repository.JsonEventDataLoad;
 import com.isa.morswiny.users.User;
+import com.isa.morswiny.web.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ejb.EJB;
 import javax.inject.Inject;
@@ -15,34 +20,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @WebServlet("/event-list")
 public class AllEventsListServlet extends HttpServlet {
 
+    private static final Logger STDOUT = LoggerFactory.getLogger(AllEventsListServlet.class);
+    private static final String TEMPLATE_NAME = "allEvents";
+    private Map<String, Object> model = new HashMap<>();
+    private List<Event> listOfMainEvents = new ArrayList<>();
+
     @Inject
     EventCRUDRepositoryInterface eventCRUDRepositoryInterface;
-
+    @Inject
+    TemplateProvider templateProvider;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
+        resp.addHeader("Content-Type", "text/html; charset=utf-8");
 
-        for (Event event : eventCRUDRepositoryInterface.getAllEventsList()) {
-            if (event != null) {
-                try {
-                    writer.write("<!DOCTYPE html><html><body>" + event + "</body></html>");
-                } catch (NullPointerException e) {
-                    writer.write("<!DOCTYPE html><html><body>" + "Event not found" + "</body></html>");
+        setModel();
+        Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
 
-                }
-            } else {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                writer.write("<!DOCTYPE html><html><body>" + "Nie ma takiego eventu." + "</body></html>");
-
-            }
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            STDOUT.error("Error while processing template: " + template.getName(), e);
         }
 
     }
+
+    private void setModel() {
+        if (model == null || model.isEmpty()) {
+            model.put("listOfMainEvents", setListOfMainEvents());
+        }
+    }
+
+    private List<Event> setListOfMainEvents() {
+        List<Event> listOfAllEvents = eventCRUDRepositoryInterface.getAllEventsList();
+        for (int i = 0; i < 3; i++) {
+            listOfMainEvents.add(listOfAllEvents.get(i));
+        }
+        return listOfMainEvents;
+    }
+
+
 }
 
 
