d5acae01b5ff8eac58821d014c0798c97a63f5dd
SudolAgata <agatasudol@o2.pl>
2020-09-05 17:04:49 +0200

    comments parser

--- src/main/java/com/isa/morswiny/servlets/AddEventServlet.java
+++ src/main/java/com/isa/morswiny/servlets/AddEventServlet.java
@@ -33,8 +33,8 @@ public class AddEventServlet extends HttpServlet {
     @Inject
     private EventCRUDRepositoryInterface eventCRUDRepositoryInterface;
 
-    @Inject
-    private DataParser dataParser;
+//    @Inject
+//    private DataParser dataParser;
 
     @Inject
     private TemplateProvider templateProvider;
