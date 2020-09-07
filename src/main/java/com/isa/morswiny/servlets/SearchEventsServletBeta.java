dfb77f762c7fbce422fe36c6ab99fc225976a768
mateuszstopyra <mateuszstopyra@gmail.com>
2020-09-08 01:22:37 +0200

    adding search-events-beta servlet and freemarker with 20ev logic per page

---
+++ src/main/java/com/isa/morswiny/servlets/SearchEventsServletBeta.java
@@ -0,0 +1,69 @@
+package com.isa.morswiny.servlets;
+
+import com.isa.morswiny.events.Event;
+import com.isa.morswiny.eventsDao.EventCRUDRepositoryInterface;
+import com.isa.morswiny.eventsDao.EventSearchRepositoryInterface;
+import com.isa.morswiny.web.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/search-events-beta")
+public class SearchEventsServletBeta extends HttpServlet {
+
+    private static final Logger STDOUT = LoggerFactory.getLogger(AllEventsListServlet.class);
+    private static final String TEMPLATE_NAME = "searchEventsBeta";
+    private String userQuery;
+    private Map<String, Object> model = new HashMap<>();
+
+    @Inject
+    EventCRUDRepositoryInterface eventCRUDRepositoryInterface;
+    @Inject
+    TemplateProvider templateProvider;
+
+    @Inject
+    EventSearchRepositoryInterface eventSearchRepositoryInterface;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        userQuery = req.getParameter("search");
+
+
+        resp.addHeader("Content-Type", "text/html; charset=utf-8");
+
+        setModel();
+        Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
+
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            STDOUT.error("Error while processing template: " + template.getName(), e);
+        }
+
+
+    }
+
+    private List<Event> setListOfQueriedEvents() {
+        return eventSearchRepositoryInterface.searchByString(userQuery);
+    }
+
+    private void setModel() {
+            model.put("listOfQueriedEvents", setListOfQueriedEvents());
+
+    }
+
+
+}
