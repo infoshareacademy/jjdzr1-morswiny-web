19209ec19d53152115d5b437723e4d84ac4d7b76
Jakub Bieńkowski <jbienkowski94@gmail.com>
2020-10-02 13:56:01 +0200

    added login servleet

---
+++ src/main/java/com/isa/morswiny/servlets/LoginServlet.java
@@ -0,0 +1,55 @@
+package com.isa.morswiny.servlets;
+
+import com.isa.morswiny.events.Event;
+import com.isa.morswiny.eventsDao.EventCRUDRepositoryInterface;
+import com.isa.morswiny.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/single-event")
+public class LoginServlet extends HttpServlet {
+
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+    private static final String TEMPLATE_NAME = "login";
+
+    @Inject
+    EventCRUDRepositoryInterface userCRUDRepositoryInterface;
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = resp.getWriter();
+        resp.addHeader("Content-Type", "text/html; charset=utf-8");
+
+        Map<String, Object> map = new HashMap<>();
+
+
+        Template template = templateProvider.createTemplate(getServletContext(), TEMPLATE_NAME);
+        try {
+            template.process(map, resp.getWriter());
+        } catch (TemplateException e) {
+            STDOUT.error("Error while processing template: ", e);
+        }
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+    }
+}
