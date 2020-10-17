e3c1589f5431bb26a8a8821bed6eb14c1733ad1e
Tomek Miecielica <tomek.miecielica@gmail.com>
2020-10-17 12:48:51 +0200

    loading to DB via streams in DbDataLoad

--- src/main/java/com/isa/morswiny/eventsDao/DbDataLoad.java
+++ src/main/java/com/isa/morswiny/eventsDao/DbDataLoad.java
@@ -5,24 +5,25 @@ import com.isa.morswiny.model.Event;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @RequestScoped
-public class DbDataLoad {
+public class DbDataLoad implements Serializable {
 
     @Inject
     private EventDao eventDao;
 
     public void loadDataToDB() {
-        List<Event> listOfEvents;
         JsonEventDataManagement jsonEventDataManagement = new JsonEventDataManagement();
-        listOfEvents = jsonEventDataManagement.createListOfAllEvents();
+        List<Event> listOfEvents = jsonEventDataManagement.createListOfAllEvents();
+        listOfEvents.forEach(event -> eventDao.save(event));
 
-
-        for (Event e : listOfEvents) {
-            eventDao.save(e);
-        }
+//
+//        for (Event e : listOfEvents) {
+//            eventDao.save(e);
+//        }
 
     }
 
