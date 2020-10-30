d1911bc2e971da07579f2efa7e46a6f8229dbf75
mateuszstopyra <mateuszstopyra@gmail.com>
2020-10-30 21:26:30 +0100

    adding favourites entity

---
+++ src/main/java/com/isa/morswiny/model/Favourites.java
@@ -0,0 +1,26 @@
+package com.isa.morswiny.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "favourites")
+public class Favourites {
+
+    @Id
+    private Integer eventId;
+    private Integer id;
+    private String endDate;
+    private String name;
+
+    @Column(length = 16777215)
+    private String descLong;
+    private String categoryId;
+    private String startDate;
+    private Integer  active;
+    private LocalDateTime startDateLDT;
+    private LocalDateTime endDateLDT;
+
+
+
+}
