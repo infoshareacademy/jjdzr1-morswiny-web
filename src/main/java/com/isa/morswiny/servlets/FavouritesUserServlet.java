27c359ef2c13e9cf186ec78d527a971abbd4166d
mateuszstopyra <mateuszstopyra@gmail.com>
2020-11-04 21:03:06 +0100

    adding logic to favourites servlet

--- src/main/java/com/isa/morswiny/servlets/FavouritesUserServlet.java
+++ src/main/java/com/isa/morswiny/servlets/FavouritesUserServlet.java
@@ -1,7 +1,13 @@
 package com.isa.morswiny.servlets;
 
+import com.isa.morswiny.dto.EventDto;
 import com.isa.morswiny.freemarker.TemplateProvider;
+import com.isa.morswiny.model.Event;
+import com.isa.morswiny.services.EventService;
 import com.isa.morswiny.services.FavouritesService;
+import com.isa.morswiny.services.UserService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +19,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //@ApplicationScoped //?
 @WebServlet("/favourites-list")
@@ -20,6 +29,20 @@ public class FavouritesUserServlet extends HttpServlet {
 
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
     private static final String TEMPLATE_NAME = "favouritesUserList";
+    private Map<String, Object> model = new HashMap<>();
+
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Inject
+    private EventService eventService;
+
+    @Inject
+    private FavouritesService favouritesService;
+
+    @Inject
+    UserService userService;
 
 
     @Override
@@ -27,8 +50,29 @@ public class FavouritesUserServlet extends HttpServlet {
 
         resp.addHeader("Content-Type", "text/html; charset=utf-8");
 
+        if (req.getSession(false) != null && req.getSession(false).getAttribute("logged") != null){
+            model.put("logged", req.getSession().getAttribute("logged"));
+        }
+
+        initModel(model, pageInt, count);
+        Template template = templateProvider.createTemplate(getServletContext(), TEMPLATE_NAME);
 
 
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            STDOUT.error("Error while processing template: " + template.getName(), e);
+        }
+
+    }
+
+    private void initModel(Map model, Integer count) {
+        model.put("listOfQueriedEvents", setListOfQueriedEvents(query));
+        model.put("count",count);
+    }
+
+    private List<EventDto> setListOfFavouritesEventsForUser(Integer id){
+        return favouritesService.getAllFavouritesForUser(id);
     }
 
 
